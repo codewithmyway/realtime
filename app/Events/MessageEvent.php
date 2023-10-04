3a711ce12ce17028ed1897932cd5cb9361dca366
vijendra <126257471+codewithmyway@users.noreply.github.com>
2023-10-04 20:51:26 +0530

    Add files via upload

---
+++ app/Events/MessageEvent.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+    public $message,$userName,$time;
+
+
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($userName,$message,$time)
+    {
+
+        $this->userName=$userName;
+        $this->message=$message;
+        $this->time=currentTime();
+
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn()
+    {
+       return new Channel('message');
+    }
+}
